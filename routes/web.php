bbd2d0865fcc70a165987f7159d5b930beea9011
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-11 18:38:20 +0200

    cambio rotte in resource

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/comics', 'ComicsController@index')->name('comics.index');
-Route::get('/comics/{{id}}', 'ComicsController@show')->name('comics.show');
+Route::resource('comics', 'ComicsController');
