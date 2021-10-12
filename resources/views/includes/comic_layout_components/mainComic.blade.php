11d1aaa49e6d86f29109d905e17a23bc587f74fe
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-12 14:10:51 +0200

    mod css + using dependes inception comics contr...

--- resources/views/includes/comic_layout_components/mainComic.blade.php
+++ resources/views/includes/comic_layout_components/mainComic.blade.php
@@ -1,21 +1,24 @@
 <div class="container">
-  <div class="row">
-    <div class="col-4">
-      <img src="{{ $comic->thumb }}" alt="">
-    </div>
-    <div class="col-8">
-      <h1>Description</h1>
-      <p>Title : <strong>{{ $comic->title }}</strong>
-      </p>
+  <div class="card mt-5">
+    <div class="row m-5">
+      <div class="col-4">
+        <img src="{{ $comic->thumb }}" alt="">
+      </div>
+      <div class="col-8">
 
-      <p>Description : <strong>{{ $comic->description }}</strong></p>
-      <p>Type: <strong>{{ $comic->type }}</strong></p>
-      <p>Series: <strong>{{ $comic->series }}</strong></p>
-      <p>Price : <strong>{{ $comic->price }}</strong></p>
-      <p>Data sale : <strong>{{ $comic->sale_date }}</strong></p>
+        <h1>Description</h1>
+        <p>Title : <strong>{{ $comic->title }}</strong>
+        </p>
+
+        <p>Description : <strong>{{ $comic->description }}</strong></p>
+        <p>Type: <strong>{{ $comic->type }}</strong></p>
+        <p>Series: <strong>{{ $comic->series }}</strong></p>
+        <p>Price : <strong>{{ $comic->price }}</strong></p>
+        <p>Data sale : <strong>{{ $comic->sale_date }}</strong></p>
 
 
-      </p>
+        </p>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/includes/comics_layout_components/main.blade.php
+++ resources/views/includes/comics_layout_components/main.blade.php
@@ -1,42 +1,47 @@
-<div class="bg-primary">
-  <div class="container pt-5">
-    <div class="text-white bg-dark text-center">
+<div class="container pt-5">
+  <div class="card">
+    <div class="card-title text-center">
       <h2 class='m-0 p-2 pb-4'>Fumetti caricati nel server</h2>
     </div>
-    <table class="table table-dark table-striped ">
-      <thead>
-        <tr>
-          <th scope="col">Title</th>
-          <th scope="col">Series</th>
-          <th scope="col">Type</th>
-          <th scope="col">Price</th>
-          <th scope="col">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @forelse($comics as $comic)
-          <tr>
 
-            <td>
-              @if ($comic->thumb)
-                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} "
-                  class="img-fluid me-2" width="20">
-              @endif
-              {{ $comic->title }}
-            </td>
-            <td>{{ $comic->series }}</td>
-            <td>{{ $comic->type }}</td>
-            <td>{{ $comic->price }}</td>
-            <td><a href="{{ route('comics.show', $comic->id) }}"
-                class="btn btn-primary">Details</a></td>
-
-          </tr>
-        @empty
+    <hr>
+    <div class="card-body">
+      <table class="table  ">
+        <thead>
           <tr>
-            <td colspan='5' class='text-center'> Non ho trovato fumetti</td>
+            <th scope="col">Title</th>
+            <th scope="col">Series</th>
+            <th scope="col">Type</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
           </tr>
-        @endforelse
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          @forelse($comics as $comic)
+            <tr>
+
+              <td>
+                @if ($comic->thumb)
+                  <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} "
+                    class="img-fluid me-2" width="20">
+                @endif
+                {{ $comic->title }}
+              </td>
+              <td>{{ $comic->series }}</td>
+              <td>{{ $comic->type }}</td>
+              <td>{{ $comic->price }}</td>
+              <td><a href="{{ route('comics.show', $comic->id) }}"
+                  class="btn btn-primary">Details</a></td>
+
+            </tr>
+          @empty
+            <tr>
+              <td colspan='5' class='text-center'> Non ho trovato fumetti</td>
+            </tr>
+          @endforelse
+        </tbody>
+      </table>
+    </div>
   </div>
+</div>
 </div>
