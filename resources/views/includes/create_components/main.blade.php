ed5893a5aee36d95eb19a5994cf20def8b7d8122
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-13 23:20:44 +0200

    create form

--- resources/views/includes/create_components/main.blade.php
+++ resources/views/includes/create_components/main.blade.php
@@ -1,4 +1,73 @@
 <div class="container">
-  io sono il form per la creazione
+  <div class="card-title mt-5">
+    <div class="d-flex justify-content-between align-items-center">
+      <h1>inserisci nuovo fumetto</h1>
+      <a href="{{ route('comics.index') }}" class="btn btn-secondary">Return</a>
+    </div>
+    <hr>
+    <div class="card-body">
+      <form action="">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="mb-3">
+              <label for="title" class="form-label">Title</label>
+              <input type="text" class="form-control" id="title" name="title">
+              <div class="form-text">Inserire Titolo Comics</div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="mb-3">
+              <label for="thumb" class="form-label">Thumb</label>
+              <input type="text" class="form-control" id="thumb" name="thumb">
+              <div class="form-text">Inserire link img copertina Comics
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label for="series" class="form-label">Series</label>
+              <input type="text" class="form-control" id="series" name="series">
+              <div class="form-text">Inserire Series Comics</div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label for="type" class="form-label">Type</label>
+              <input type="text" class="form-control" id="type" name="type">
+              <div class="form-text">Inserire type Comics</div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label for="sale_date" class="form-label">Sale date</label>
+              <input type="date" class="form-control" id="sale_date" name="sale_date">
+              <div class="form-text">Inserire data di vendita Comics</div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label for="price" class="form-label">Price</label>
+              <input type="number" class="form-control" id="price" name="price">
+              <div class="form-text">Inserire prezzo di vendita Comics </div>
+            </div>
+          </div>
+          <div class="col 12">
+            <div class="mb-3">
+              <label for="description" class="form-label">Description</label>
+              <textarea class="form-control" id="description" rows="5" name='description'></textarea>
+              <div class="form-text">Inserire descrizione Comics </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer bg-white d-flex justify-content-between">
+          <button type="reset" class='btn btn-secondary'>Reset</button>
+          <button type="submit" class='btn btn-success'>Create</button>
+
+        </div>
+    </div>
+  </div>
+  </form>
+</div>
+</div>
 
 </div>
