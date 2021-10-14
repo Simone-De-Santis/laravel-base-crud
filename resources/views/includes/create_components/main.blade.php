a46498acae0c2f2dc48be591acef2627149a3ce0
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-14 17:33:37 +0200

    ''''validazione lato html''''

--- resources/views/includes/create_components/main.blade.php
+++ resources/views/includes/create_components/main.blade.php
@@ -12,14 +12,14 @@
           <div class="col-md-6">
             <div class="mb-3">
               <label for="title" class="form-label">Title</label>
-              <input type="text" class="form-control" id="title" name="title">
+              <input type="text" class="form-control" id="title" name="title" required>
               <div class="form-text">Inserire Titolo Comics</div>
             </div>
           </div>
           <div class="col-md-6">
             <div class="mb-3">
               <label for="thumb" class="form-label">Thumb</label>
-              <input type="text" class="form-control" id="thumb" name="thumb">
+              <input type="text" class="form-control" id="thumb" name="thumb" required>
               <div class="form-text">Inserire link img copertina Comics
               </div>
             </div>
@@ -27,28 +27,28 @@
           <div class="col-md-3">
             <div class="mb-3">
               <label for="series" class="form-label">Series</label>
-              <input type="text" class="form-control" id="series" name="series">
+              <input type="text" class="form-control" id="series" name="series" required>
               <div class="form-text">Inserire Series Comics</div>
             </div>
           </div>
           <div class="col-md-3">
             <div class="mb-3">
               <label for="type" class="form-label">Type</label>
-              <input type="text" class="form-control" id="type" name="type">
+              <input type="text" class="form-control" id="type" name="type" required>
               <div class="form-text">Inserire type Comics</div>
             </div>
           </div>
           <div class="col-md-3">
             <div class="mb-3">
               <label for="sale_date" class="form-label">Sale date</label>
-              <input type="date" class="form-control" id="sale_date" name="sale_date">
+              <input type="date" class="form-control" id="sale_date" name="sale_date" required>
               <div class="form-text">Inserire data di vendita Comics</div>
             </div>
           </div>
           <div class="col-md-3">
             <div class="mb-3">
               <label for="price" class="form-label">Price</label>
-              <input type="number" class="form-control" id="price" name="price">
+              <input type="number" class="form-control" id="price" name="price" required>
               <div class="form-text">Inserire prezzo di vendita Comics </div>
             </div>
           </div>
