55359b0d3f3fc285de17b29f26adad6076a8d773
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-14 18:59:22 +0200

    create page for edit

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,17 @@
+{{-- layout di riferimento --}}
+@extends('layout.main')
+
+{{-- titolo pagina --}}
+@section('title', 'edit')
+
+{{-- sezione header --}}
+@section('header_id', 'edit-header')
+@section('header')
+  @include('includes/generics_component/header')
+@endsection
+
+{{-- sezione contenuto main --}}
+@section('main_id', 'edit-main')
+@section('content')
+  @include('includes/edit_components/main')
+@endsection
