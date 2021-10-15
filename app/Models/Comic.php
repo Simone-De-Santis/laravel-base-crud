d4011b91e4f1e72db63db6376da5e69e79738949
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-15 19:26:52 +0200

    creata migration per soft delete

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -3,11 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// aggiunta per soft delite (cestino)
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Comic extends Model
 {
     //
-    //! campi per la funzione fill
+    use SoftDeletes;
 
+
+    //! campi per la funzione fill
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type', 'slug'];
 }
+
+
+
+
+
+// per aggiungere la colonna nel db per il soft delite eseguire una migration per crearla nel db
+// php artisan make:migration add_deleted_at_to_comics_table --table=comics 
